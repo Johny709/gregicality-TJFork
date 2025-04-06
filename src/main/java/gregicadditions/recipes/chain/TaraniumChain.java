f2b7ccc0f792ea77466d6ed14487e66779e88ae9
StaffiX <32968022+StaffiX@users.noreply.github.com>
2025-04-06 07:40:44 +0200

    Fix Taranium Advanced Fusion recipe (#28)
    * fix taranium advanced fusion recipe

--- src/main/java/gregicadditions/recipes/chain/TaraniumChain.java
+++ src/main/java/gregicadditions/recipes/chain/TaraniumChain.java
@@ -287,7 +287,7 @@ public class TaraniumChain {
                 .buildAndRegister();
 
 
-        ADV_FUSION_RECIPES.recipeBuilder().duration(100).EUt(16000000).coilTier(1).euStart(1200000000L).euReturn(75)
+        ADV_FUSION_RECIPES.recipeBuilder().duration(100).EUt(120000).coilTier(2).euStart(1200000000L).euReturn(75)
                 .fluidInputs(TaraniumEnrichedLHelium3.getFluid(1000))
                 .fluidInputs(Helium3.getFluid(1000))
                 .fluidOutputs(TaraniumRichDustyHeliumPlasma.getFluid(3000))
